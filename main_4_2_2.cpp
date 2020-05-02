775872cecd816c8f55eb4e1c3a95c87a89ec795c
Папа <papa@iMac-Ruslan.local>
2020-05-02 09:39:23 +0300

    4 2 2

---
+++ main_4_2_2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+vector<int> used(20001, 0);
+list<int> visit;
+vector<int> component(20001, 0);
+
+void dfs(const vector<vector<int> >& graf, int v, int l)
+{
+	used[v] = 1;
+	//visit.push_back(l);
+	component[v] = l;
+	for (auto u : graf[v])
+	{
+		if (used[u])
+			continue;
+		dfs(graf, u, l);
+	}
+}
+
+int main()
+{
+	int n;
+	int m;
+	int i;
+	int j;
+	int s_size;
+	int u;
+	int v;
+	int l; //quant of component
+
+	cin >> n >> m;
+    //n = 5; m = 7;
+	vector <vector <int> > graf(n + 1);
+	i = 0;
+	while (++i <= m)
+	{
+		cin >> u >> v;
+		graf[u].push_back(v);
+		graf[v].push_back(u);
+	}
+	cout << "size=" << graf.size() << '\n';
+	i = 0;
+	while (++i <= n)
+	{
+		cout << i << " : ";
+		for(auto i2 : graf[i])
+			cout << i2 << " ";
+		cout << "\n";
+	}
+	i = 0;
+	l = 0;
+	while (++i <= n)
+	{
+		if (!used[i])
+		{
+			++l;
+			dfs(graf, i, l);
+		}
+	}
+	cout << l << "\n";
+	i = 0;
+	while (++i <= n)
+		cout << component[i] << " ";
+//	for (auto u: visit)
+//		cout << u << " ";
+	return 0;
+}
