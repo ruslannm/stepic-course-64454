99503ea31a8f0733b6eb560ff77f92955402928b
Папа <papa@iMac-Ruslan.local>
2020-05-04 22:21:09 +0300

    4 2 8

---
+++ main_4_2_8.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> used(501, 0);
+int cicle;
+int start_cicle;
+vector<int> solve(501, 0);
+
+void dfs(const vector<vector<int> >& graf, int to, int from)
+{
+	used[to] = 1;
+	solve.push_back(to);
+	for (auto u : graf[to])
+	{
+		if (1 == cicle)
+			return;
+		if (1 == used[u] && u != from)
+		{
+			cicle = 1;
+			start_cicle = u;
+		}
+		else if (0 == used[u])
+			dfs(graf, u, to);
+	}
+	used[to] = 2;
+}
+
+int main()
+{
+	int n;
+	int m;
+	int i;
+	int u;
+	int v;
+
+	cin >> n >> m;
+	vector <vector <int> > graf(n + 1);
+	i = 0;
+	while (++i <= m)
+	{
+		cin >> u >> v;
+		graf[u].push_back(v);
+		graf[v].push_back(u);
+	}
+	cicle = 0;
+	i = 0;
+	while (++i <= n)
+	{
+		used[0] = 0;
+		for (int j = 1; j <= n; ++j)
+			used[j] = 0;
+		dfs(graf, i, 0);
+		if (cicle)
+		{
+			int j = solve.size();
+			int q = 1;
+			while (solve[j - q] != start_cicle)
+				++q;
+			cout << "YES\n" << q << "\n";
+			while (q)
+			{
+				cout << solve[j - q] << " ";
+				--q;
+			}
+			return 0;
+		}
+	}
+	cout << "NO";
+	return 0;
+}
